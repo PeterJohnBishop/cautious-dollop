6ea91728aa876d543f077577856eee34c29da190
PeterB <peterjbishop.denver@gmail.com>
2025-06-05 00:44:42 -0600

    resolved issue with upload server and postgres startup conflict

--- pgdb/postgres.go
+++ pgdb/postgres.go
@@ -4,10 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 )
 
 func ConnectPSQL(db *sql.DB) *sql.DB {
-
 	initEnv()
 
 	host := postgresHost
@@ -15,21 +15,31 @@ func ConnectPSQL(db *sql.DB) *sql.DB {
 	user := postgresUser
 	password := postgresPassword
 	dbname := postgresDBName
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	// fmt.Println("Connecting with:", psqlInfo)
 
-	mydb, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	var mydb *sql.DB
+	var err error
+	maxAttempts := 10
+
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		mydb, err = sql.Open("postgres", psqlInfo)
+		if err == nil {
+			err = mydb.Ping()
+		}
+
+		if err == nil {
+			log.Printf("[CONNECTED] to Postgres on %s:%s", host, port)
+			return mydb
+		}
+
+		log.Printf("[RETRY %d/%d] Could not connect to Postgres: %v", attempt, maxAttempts, err)
+		time.Sleep(2 * time.Second)
 	}
 
-	err = mydb.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	log.Printf("[CONNECTED] to Postgres on :%s", port)
-	return mydb
+	log.Fatalf("Failed to connect to Postgres after %d attempts: %v", maxAttempts, err)
+	return nil
 }
